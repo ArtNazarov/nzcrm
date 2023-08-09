0b26e74c4a58b92b2babfa69afd3912b01ca6dc5
Artem Nazarov <programmist.nazarov@gmail.com>
2023-08-09 16:24:09 +0500

    Documention with phpDocumentor

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -5,6 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Client
+ * @package app/Models
+ * 
+ * @property $timestamp use timestamp in models, should be false 
+ */
 class Client extends Model
 {
     use HasFactory;
---
+++ gen-doc.sh
@@ -0,0 +1,1 @@
+phpdoc -d . -t ~/laranotes/docs
